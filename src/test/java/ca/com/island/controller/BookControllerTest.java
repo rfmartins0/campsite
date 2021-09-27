0c2e8cd406ed35fb6055d363b0250c60c7ce3e90
Rafael Franco Martins <francomartins@gmail.com>
2021-09-27 16:39:29 -0300

    change BookControllerTest

--- src/test/java/ca/com/island/controller/BookControllerTest.java
+++ src/test/java/ca/com/island/controller/BookControllerTest.java
@@ -1,5 +1,6 @@
 package ca.com.island.controller;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.net.URI;
@@ -13,8 +14,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.com.island.dto.ClientBookDto;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -32,6 +37,34 @@ public class BookControllerTest {
 		URI uri = new URI(baseUrl);
 		ResponseEntity<Object> response = this.restTemplate.getForEntity(uri, Object.class);
 		assertTrue(response.getStatusCode().is2xxSuccessful());
+	}
+	
+	@Test
+	public void testBookingCode() throws URISyntaxException {
+		final String baseUrl = "http://localhost:" + port + "/books";
+		URI uri = new URI(baseUrl);
+		ClientBookDto clientBookDto = new ClientBookDto();
+		clientBookDto.setEmail(baseUrl);
+		clientBookDto.setEndDate(LocalDate.now().plusDays(2l));
+		clientBookDto.setStartDate(LocalDate.now().plusDays(200l));
+		clientBookDto.setName("Neil XYZ");
+		HttpEntity<ClientBookDto> request = new HttpEntity<>(clientBookDto);
+		ResponseEntity<String> result = this.restTemplate.postForEntity(uri, request, String.class);
+		assertEquals(HttpStatus.BAD_REQUEST, result.getStatusCode());
+	}
+	
+	@Test
+	public void testBookingMessage() throws URISyntaxException {
+		final String baseUrl = "http://localhost:" + port + "/books";
+		URI uri = new URI(baseUrl);
+		ClientBookDto clientBookDto = new ClientBookDto();
+		clientBookDto.setEmail(baseUrl);
+		clientBookDto.setEndDate(LocalDate.now().plusDays(2l));
+		clientBookDto.setStartDate(LocalDate.now().plusDays(200l));
+		clientBookDto.setName("Neil XYZ");
+		HttpEntity<ClientBookDto> request = new HttpEntity<>(clientBookDto);
+		ResponseEntity<String> result = this.restTemplate.postForEntity(uri, request, String.class);
+		assertEquals("{\"message\":\"Invalid date\"}", result.getBody());
 	}
 
 }
